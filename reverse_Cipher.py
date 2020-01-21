e5120d65dfcfb2a1ac16c425048c5c7ca1c7f8ee
mansoorinho <man.nabavi96@gmail.com>
2020-01-20 22:27:36 -0800

    asks for user input

--- reverse_Cipher.py
+++ reverse_Cipher.py
@@ -12,11 +12,16 @@ while i>= 0:
 print(translated)
 
 #### another way of reverse cipher by me
-new_msg = "this should work for reverse cipher"
-j = -(len(new_msg))
-rev_msg = ""
-n=-1
-while (n >= j):
-    rev_msg = rev_msg + new_msg[n]
-    n = n - 1
-print("new cipher result: ", rev_msg)
+def reverse_function (new_msg):
+    ##new_msg="this new approach works too"
+    j = -(len(new_msg))
+    rev_msg = ""
+    n=-1
+    while (n >= j):
+        rev_msg = rev_msg + new_msg[n]
+        n = n - 1
+    print("new cipher result: ", rev_msg)
+
+### asking for user input to be reversed
+user_input = input("Insert your message to be reversed: ")
+cipher = reverse_function(user_input)
