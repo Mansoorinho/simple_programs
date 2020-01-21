cc647a512a0b77f6dec2738643b8a13e6c62c588
mansoorinho <man.nabavi96@gmail.com>
2020-01-20 22:15:56 -0800

    new approach added to reverse cipher

--- reverse_Cipher.py
+++ reverse_Cipher.py
@@ -10,3 +10,13 @@ while i>= 0:
     i = i -1
 
 print(translated)
+
+#### another way of reverse cipher by me
+new_msg = "this should work for reverse cipher"
+j = -(len(new_msg))
+rev_msg = ""
+n=-1
+while (n >= j):
+    rev_msg = rev_msg + new_msg[n]
+    n = n - 1
+print("new cipher result: ", rev_msg)
