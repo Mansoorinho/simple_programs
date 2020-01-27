0697ec0ef1a9175c9581ad3b91081572dcf3078f
mansoorinho <man.nabavi96@gmail.com>
2020-01-27 01:09:00 -0800

    Transposition cipher Decryption completed

--- TranspostionCipher_Decryption.py
+++ TranspostionCipher_Decryption.py
@@ -5,10 +5,27 @@
 import math, pyperclip
 def main():
     myMessage="Trheits  Miess saa gSeec"
-    myKey=8
+    myKey= 13
     plaintext = decryptMessage(myKey,myMessage)
     print(plaintext+"|")
     pyperclip.copy(plaintext)
 
 def decryptMessage(key, message):
-    
+    numOfColumns = int(math.ceil(len(message)/float(key)))
+    numOfRows = key
+    numOfShadedBoxes = (numOfColumns*numOfRows) - len(message)
+
+    plaintext=[""]*numOfColumns
+    column = 0
+    row = 0
+    for symbol in message:
+        plaintext[column] += symbol
+        column +=1
+
+        if (column == numOfColumns) or (column == numOfColumns - 1 and row >= numOfRows - numOfShadedBoxes):
+            column = 0
+            row += 1
+    return "".join(plaintext)
+
+if __name__ == "__main__":
+    main()
