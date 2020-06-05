a6057bf94375422de2a929f3e38a9b305eea2f36
mansoorinho <man.nabavi96@gmail.com>
2020-06-05 04:21:53 -0700

    Updated, now can find the closest match

--- appDict.py
+++ appDict.py
@@ -2,23 +2,29 @@
 A simple English dictionary created by Mansoor Nabawi on Jun 5
 """
 import json
-from difflib import SequenceMatcher as SQ
+#from difflib import SequenceMatcher as SQ
 from difflib import get_close_matches as gcm
-
+#loading the dictionary as data
 data = json.load(open("data.json","r"))
-print(type(data))
 
 def translate(word: str)-> str:
+    """
+    This function get a word as an input and search for its meaning\
+    if the word entered didn't exactly match to the dictionary\
+    it looks for the closest match and asks user's opinion.
+    """
     word = word.lower()
     if word in data:
         return data[word]
     elif gcm(word, data.keys(),cutoff=0.8):
         Possib =  gcm(word, data.keys())[0]
-        posibWord = input (f"Did you mean '{Possib}'' ? ")
-        if posibWord == "Y":
+        UserAnswer = input (f"Did you mean '{Possib}' ? 'Y' for yes 'N' for no: ")
+        if UserAnswer == "Y":
             return translate(Possib)
+        elif UserAnswer == "N":
+            return f"The word '{word} does not exist, please double check"
         else:
-            return "Good bye"
+            return "We didn't uderstand your query!"
     else:
         return f"the word '{word}' does not exist, please double check it"
 
